cbe3b32ce92fd049cc610482e61d942d05398f52
Xiverl <den.elynin0903@gmail.com>
2025-07-22 20:55:24 +0400

    Add alembic and postgres connect.

--- src/db.py
+++ src/db.py
@@ -1,14 +1,15 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import declarative_base, sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./sql_app.db"
+from src.config import settings
 
 engine = create_async_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    url=settings.DATABASE_URL_asyncpg
 )
 
 AsyncSessionLocal = sessionmaker(
-    engine, class_=AsyncSession, expire_on_commit=False
+    engine, class_=AsyncSession,
+    expire_on_commit=False
 )
 
 Base = declarative_base()
